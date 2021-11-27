ad620fb14b87fc427b096e5e5a94f7224678f7e5
Yan Kaiky <yankaikys@gmail.com>
2021-11-27 18:16:32 -0300

    feat(Routes): resource

--- start/routes.ts
+++ start/routes.ts
@@ -4,34 +4,10 @@ Route.get('/', async () => {
   return { hello: 'world' }
 })
 
-Route.group(() => {
-  Route.post('', 'UsersController.store')
-  Route.get('', 'UsersController.index')
-  Route.get(':/guid', 'UsersController.show')
-  Route.put(':/guid', 'UsersController.update')
-  Route.delete(':/guid', 'UsersController.destroy')
-}).prefix('/users')
+Route.resource('users', 'UsersController').apiOnly();
 
-Route.group(() => {
-  Route.post('', 'AnimesController.store')
-  Route.get('', 'AnimesController.index')
-  Route.get('/:guid', 'AnimesController.show')
-  Route.put('/:guid', 'AnimesController.update')
-  Route.delete('/:guid', 'AnimesController.destroy')
-}).prefix('/animes')
+Route.resource('animes', 'AnimesController').apiOnly();
 
-Route.group(() => {
-  Route.post('', 'MoviesController.store')
-  Route.get('', 'MoviesController.index')
-  Route.get('/:guid', 'MoviesController.show')
-  Route.put('/:guid', 'MoviesController.update')
-  Route.delete('/:guid', 'MoviesController.destroy')
-}).prefix('/movies')
+Route.resource('movies', 'MoviesController').apiOnly();
 
-Route.group(() => {
-  Route.post('', 'SeriesController.store')
-  Route.get('', 'SeriesController.index')
-  Route.get('/:guid', 'SeriesController.show')
-  Route.put('/:guid', 'SeriesController.update')
-  Route.delete('/:guid', 'SeriesController.destroy')
-}).prefix('/series')
+Route.resource('series', 'SeriesController').apiOnly();
